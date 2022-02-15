8888495d37e38013a6d98d0a72beb9c2846c0ec9
jiweon <kor98won@gmail.com>
2022-02-16 01:34:54 +0900

    Hello World! 서버 프로그램 구현

---
+++ ch01_network_programming_and_socket/hello_server.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handling(char* message);
+
+int main(int argc, char* argv[]) {
+    int serv_sock, clnt_sock;
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+
+    char message[] = "Hello world!";
+
+    if (argc != 2) {
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    // 소켓 생성
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (serv_sock == -1)
+        error_handling("socket error");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    // bind로 IP 주소와 PORT 번호 할당
+    if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("bind() error");
+
+    // listen 함수를 호출해 소켓이 연결요청을 받아들일 수 있게 한다.
+    if (listen(serv_sock, 5) == 1)
+        error_handling("listen() error");
+
+    clnt_addr_size = sizeof(clnt_addr);
+    // 연결요청의 수락을 위한 accept 함수 호출,
+    // 연결요청이 없는 상태에서 이 함수가 호출되면, 연결 요청이 있을 때까지 기다린다.
+    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+    if (clnt_sock == -1)
+        error_handling("accept() error");
+
+    write(clnt_sock, message, sizeof(message));
+    close(clnt_sock);
+    close(serv_sock);
+    return 0;
+}
+
+void error_handling(char* message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
