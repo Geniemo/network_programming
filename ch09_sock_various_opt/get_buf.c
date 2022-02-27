c5805f9e84820ccffff0f47ade1264fa596f64a7
park <kor98won@gmail.com>
2022-02-27 23:01:02 +0900

    correct getbuf typo

--- ch09_sock_various_opt/get_buf.c
+++ ch09_sock_various_opt/get_buf.c
@@ -11,19 +11,20 @@ int main(int argc, char* argv[]) {
     socklen_t len;
 
     sock = socket(PF_INET, SOCK_STREAM, 0);
-    len = sizeof(snd_buf);
-    state = getsockopt(sock, SOL_SOCKET, SO_SNDBUF, (void*)&rcv_buf, &len);
-    if (state)
-        error_handling("getsockopt() error");
 
     len = sizeof(rcv_buf);
     state = getsockopt(sock, SOL_SOCKET, SO_RCVBUF, (void*)&rcv_buf, &len);
     if (state)
         error_handling("getsockopt() error");
     
+    len = sizeof(snd_buf);
+    state = getsockopt(sock, SOL_SOCKET, SO_SNDBUF, (void*)&snd_buf, &len);
+    if (state)
+        error_handling("getsockopt() error");
+    
     printf("Input buffer size: %d\n", rcv_buf);
     printf("Output buffer size: %d\n", snd_buf);
-    
+
     return 0;
 }
 
